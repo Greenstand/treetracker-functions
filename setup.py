6795ec99dcfd91d5293216e4cadf7fccca5e2aff
Philip Boadu Asare <asarephilip8@gmail.com>
2023-10-18 13:41:59 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='treetracker-functions',
+    version='1.0',
+    description='Functions to be used in treetracker DAG',
+    packages=['python/lib'],
+    scripts=['scripts'],
+)
